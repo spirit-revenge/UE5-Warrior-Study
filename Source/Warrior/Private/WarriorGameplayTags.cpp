45e7bfc00a9b7eb777c2c59b3bd9585fe41d0620
滕越 <tengyue@tengyuedeMacBook-Air.local>
2025-09-14 15:02:59 +0900

    finish:hero的轻攻击与重攻击

--- Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -11,14 +11,21 @@ namespace WarriorGameplayTags
 	/** Input Tags **/
 	UE_DEFINE_GAMEPLAY_TAG(InputTags_Move,"InputTag.Move");
 	UE_DEFINE_GAMEPLAY_TAG(InputTags_Look,"InputTag.Look");
+	
 	UE_DEFINE_GAMEPLAY_TAG(InputTags_EquipAxe,"InputTag.EquipAxe");
 	UE_DEFINE_GAMEPLAY_TAG(InputTags_UnequipAxe,"InputTag.UnequipAxe");
+
+	UE_DEFINE_GAMEPLAY_TAG(InputTags_LightAttack_Axe,"InputTag.LightAttack.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(InputTags_HeavyAttack_Axe,"InputTag.HeavyAttack.Axe");
 
 	/** Player Tags **/
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Equip_Axe,"Player.Ability.Equip.Axe");
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Unequip_Axe,"Player.Ability.Unequip.Axe");
 
-	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe,"Player.Weapon.Axe")
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Light_Axe,"Player.Ability.Attack.Light.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Heavy_Axe,"Player.Ability.Attack.Heavy.Axe");
+	
+	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe,"Player.Weapon.Axe");
 	
 	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Equip_Axe,"Player.Event.Equip.Axe");
 	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Unequip_Axe,"Player.Event.Unequip.Axe");
