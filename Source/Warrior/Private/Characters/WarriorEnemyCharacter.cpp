da8595b581409abfc8c7d110a9bc23217f4510de
滕越 <tengyue@tengyuedeMacBook-Air.local>
2025-09-15 22:39:17 +0900

    add:添加了enemy基本内容

---
+++ Source/Warrior/Private/Characters/WarriorEnemyCharacter.cpp
@@ -0,0 +1,57 @@
+// Spirit Revenge All Rights Reserved
+
+
+#include "Characters/WarriorEnemyCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/Combat/EnemyCombatComponent.h"
+#include "Engine/AssetManager.h"
+#include "DataAssets/StartUpData/DataAsset_EnemyStartUpDataBase.h"
+
+#include "WarriorDebugHelper.h"
+
+AWarriorEnemyCharacter::AWarriorEnemyCharacter()
+{
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement() -> bUseControllerDesiredRotation = false;
+	GetCharacterMovement() -> bOrientRotationToMovement = false;
+	GetCharacterMovement() -> RotationRate = FRotator(0.f,180.f,0.f);
+	GetCharacterMovement() -> MaxWalkSpeed = 300.f;
+	GetCharacterMovement() -> BrakingDecelerationWalking = 1000.f;
+
+	EnemyCombatComponent = CreateDefaultSubobject<UEnemyCombatComponent>("EnemyCombatComponent");
+}
+
+void AWarriorEnemyCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	InitEnemyStartUpData();
+}
+
+void AWarriorEnemyCharacter::InitEnemyStartUpData()
+{
+	if (CharacterStartUpData.IsNull())
+	{
+		return;
+	}
+
+	UAssetManager::GetStreamableManager().RequestAsyncLoad(
+		CharacterStartUpData.ToSoftObjectPath(),
+		FStreamableDelegate::CreateLambda(
+			[this]()
+			{
+				if(UDataAsset_StartUpDataBase* LoadedData = CharacterStartUpData.Get())
+				{
+					LoadedData -> GiveToAbilitySystemComponent(WarriorAbilitySystemComponent);
+
+					Debug::Print(TEXT("Enemy Start Up Data Loaded"),FColor::Green);
+				}
+			}
+		)
+	);
+}
---
+++ Source/Warrior/Public/Characters/WarriorEnemyCharacter.h
@@ -0,0 +1,35 @@
+// Spirit Revenge All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "WarriorEnemyCharacter.generated.h"
+
+class UEnemyCombatComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorEnemyCharacter : public AWarriorBaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AWarriorEnemyCharacter();
+
+protected:
+	//~ Begin APawn Interface.
+	virtual void PossessedBy(AController* NewController) override;
+	//~ End APawn Interface
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat")
+	UEnemyCombatComponent* EnemyCombatComponent;
+
+private:
+	void InitEnemyStartUpData();
+	
+public:
+	FORCEINLINE UEnemyCombatComponent* GetEnemyCombatComponent() const { return EnemyCombatComponent; }
+};
