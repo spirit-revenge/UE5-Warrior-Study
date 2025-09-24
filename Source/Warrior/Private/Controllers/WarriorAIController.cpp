ad70b5fbc34f46a0e5ab503d5ca74b5a70cdbed4
spirit-revenge <www.2861684963@qq.com>
2025-09-24 22:10:02 +0900

    add:添加了AI的行为树控制enemy的基本逻辑

---
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -0,0 +1,81 @@
+// Spirit Revenge All Rights Reserved
+
+
+#include "Controllers/WarriorAIController.h"
+
+#include "WarriorDebugHelper.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
+{
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("EnemySenseConfig_Sight");
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	AISenseConfig_Sight->SightRadius = 5000.f;
+	AISenseConfig_Sight->LoseSightRadius = 0.f;
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("EnemyPerceptionComponent");
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnEnemyPerceptionUpdated);
+
+	AAIController::SetGenericTeamId(FGenericTeamId(1));
+}
+
+ETeamAttitude::Type AWarriorAIController::GetTeamAttitudeTowards(const AActor& Other) const
+{
+	const APawn* PawnToCheck = Cast<const APawn>(&Other);
+
+	const IGenericTeamAgentInterface* OtherTeamAgent = Cast<const IGenericTeamAgentInterface>(PawnToCheck->GetController());
+
+	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() != GetGenericTeamId())
+	{
+		return ETeamAttitude::Hostile;
+	}
+
+	return ETeamAttitude::Friendly;
+}
+
+void AWarriorAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		//Debug::Print(TEXT("CrowdFollowingComponent"), FColor::Green);
+
+		CrowdComp->SetCrowdSimulationState(bEnableDetourCrowdAvoidance ? ECrowdSimulationState::Enabled : ECrowdSimulationState ::Disabled);
+
+		switch (DetourCrowdAvoidanceQuality)
+		{
+		case 1: CrowdComp->SetCrowdAvoidanceQuality( ECrowdAvoidanceQuality::Low); break;
+		case 2: CrowdComp->SetCrowdAvoidanceQuality( ECrowdAvoidanceQuality::Medium); break;
+		case 3: CrowdComp->SetCrowdAvoidanceQuality( ECrowdAvoidanceQuality::Good); break;
+		case 4: CrowdComp->SetCrowdAvoidanceQuality( ECrowdAvoidanceQuality::High); break;
+		default: break;	
+		}
+
+		CrowdComp->SetAvoidanceGroup(1);
+		CrowdComp->SetGroupsToAvoid(1);
+		CrowdComp->SetCrowdCollisionQueryRange(CollisionQueryRange);
+	}
+}
+
+void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	if (Stimulus.WasSuccessfullySensed() && Actor)
+	{
+		//Debug::Print(Actor->GetActorNameOrLabel() + TEXT(" was sensed"), FColor::Green);
+
+		if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+		{
+			BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+		}
+	}
+}
--- Source/Warrior/Public/Controllers/WarriorHeroController.h
+++ Source/Warrior/Public/Controllers/WarriorHeroController.h
@@ -4,14 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "GenericTeamAgentInterface.h"
 #include "WarriorHeroController.generated.h"
 
 /**
  * 玩家控制器类，用于接收输入、驱动角色、管理 HUD 等
  */
 UCLASS()
-class WARRIOR_API AWarriorHeroController : public APlayerController
+class WARRIOR_API AWarriorHeroController : public APlayerController, public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()//自动生成反射和序列化代码
-	
+
+public:
+	AWarriorHeroController();
+
+	//~ Begin IGenericTeamAgentInterface Interface.
+	virtual FGenericTeamId GetGenericTeamId() const override;
+	//~ End IGenericTeamAgentInterface Interface.
+private:
+	FGenericTeamId HeroTeamID;
 };
