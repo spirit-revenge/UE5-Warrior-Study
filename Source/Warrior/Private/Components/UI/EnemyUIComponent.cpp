53d22b2cbc4fa8b1523028b4d8c0db889038dff0
spirit-revenge <www.2861684963@qq.com>
2025-10-03 13:09:54 +0900

    add:添加了boss enemy和boss widget以及boss死亡的能力

--- Source/Warrior/Private/Components/UI/EnemyUIComponent.cpp
+++ Source/Warrior/Private/Components/UI/EnemyUIComponent.cpp
@@ -3,3 +3,25 @@
 
 #include "Components/UI/EnemyUIComponent.h"
 
+#include "Widgets/WarriorWidgetBase.h"
+
+void UEnemyUIComponent::RegisterEnemyDrawWidget(UWarriorWidgetBase* InWidgetToRegister)
+{
+	EnemyDrawWidgets.Add(InWidgetToRegister);
+}
+
+void UEnemyUIComponent::RemoveEnemyDrawWidgetIfAny()
+{
+	if (EnemyDrawWidgets.IsEmpty())
+	{
+		return;
+	}
+
+	for (UWarriorWidgetBase* DrawWidget : EnemyDrawWidgets)
+	{
+		if (DrawWidget)
+		{
+			DrawWidget -> RemoveFromParent();
+		}
+	}
+}
--- Source/Warrior/Public/Components/UI/EnemyUIComponent.h
+++ Source/Warrior/Public/Components/UI/EnemyUIComponent.h
@@ -6,6 +6,7 @@
 #include "Components/UI/PawnUIComponent.h"
 #include "EnemyUIComponent.generated.h"
 
+class UWarriorWidgetBase;
 /**
  * 
  */
@@ -13,5 +14,13 @@ UCLASS()
 class WARRIOR_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawWidget(UWarriorWidgetBase* InWidgetToRegister);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawWidgetIfAny();
+private:
+	TArray<UWarriorWidgetBase*> EnemyDrawWidgets;
 };
