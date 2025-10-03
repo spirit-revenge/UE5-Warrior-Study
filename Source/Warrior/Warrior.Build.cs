102f6cc45710b3536939363d6c6068e3259ad572
spirit-revenge <www.2861684963@qq.com>
2025-10-03 22:33:08 +0900

    add: 添加了Giant的右手攻击、连击以及召唤Guardian的能力

--- Source/Warrior/Warrior.Build.cs
+++ Source/Warrior/Warrior.Build.cs
@@ -20,7 +20,8 @@ public class Warrior : ModuleRules
 			"GameplayTasks",
 			"AnimGraphRuntime",
 			"MotionWarping",
-			"Niagara"
+			"Niagara",
+			"NavigationSystem"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
